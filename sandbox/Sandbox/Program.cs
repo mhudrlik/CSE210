d1967fa8ff9b8d5531614193c96212a29a6a3658
mhudrlik <matt@hudrlik.com>
2023-04-21 11:04:50 -0600

    Week 1 Prove Variables

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        //Defined Types
+        //Declare then initialize
+        int x;
+        x = 5;
+        Console.WriteLine(x);
+
+        //Declare and initialize in one statement
+        int y = 6;
+        Console.WriteLine(y);
+
+        //Common Types
+        //int, string, float, double, bool
+
+        //Input and Output
+        //Output - No new line afterward
+        Console.Write("There will not be a newline after this.");
+        //Output - Including new line
+        Console.WriteLine("Hello World!");
+
+        //Input
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+
+        //Blocks
+        if (x < y)
+            {
+                Console.WriteLine("greater");
+            }
+
+        //String Interpolation
+        string school = "BYU-Idaho";
+        Console.WriteLine($"I am studying at {school}.");
     }
 }
